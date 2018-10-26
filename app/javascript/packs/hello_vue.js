99915aa8c5932246a154c1a982fad0a9cbb3f4f2
kousukeuo <s1611427@s.tsukuba.ac.jp>
2018-10-26 15:37:06 +0900

    モデル連携の準備

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,31 +1,41 @@
 import Vue from 'vue/dist/vue.esm'
 import axios from 'axios';
 
+
 document.addEventListener('DOMContentLoaded', () => {
 
+  //変数argにgetメソッドのパラメータを格納
+  //arg.template_idとしてアクセス。
+  var arg = new Object;
+  var pair=location.search.substring(1).split('&');
+  for(var i=0;pair[i];i++) {
+    var kv = pair[i].split('=');
+    arg[kv[0]]=kv[1];
+  }
+
+  //ここからvueインスタンス
   const app = new Vue({
     el: '#app',
     data: {
       answer: "",
       note: "",
-      question: []
+      question:""
     },
-    methods: {
 
+    mounted: function () {
+      axios.get(`api/questions/${arg.template_id}`)
+           .then(res => {
+             this.question = res.data;
+           });
+    },
+
+    methods: {
       addAnswerToNote: function (){
-        this.note += this.question[0]
+        this.note += this.question.title
         this.note += "\n"
         this.note += this.answer
         this.note += "\n"
         this.answer = ""
-      },
-      hoge: function(id){
-        axios.get(`api/questions/${id}`)
-             .then(res => {
-               this.question.push(res.data.title);
-               this.addAnswerToNote();
-             });
-
       }
     }
   })
