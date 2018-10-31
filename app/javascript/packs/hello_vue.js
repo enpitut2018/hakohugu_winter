86b880935f84f5198504c129f37fc166a3d1f349
kousukeuo <s1611427@s.tsukuba.ac.jp>
2018-10-31 13:01:22 +0900

    4

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -4,16 +4,16 @@
 // to the head of your layout file,
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
-/* import Vue from 'vue'
- * import App from '../app.vue'
- * document.addEventListener('DOMContentLoaded', () => {
- *   const el = document.body.appendChild(document.createElement('hello'))
- *   const app = new Vue({
- *     el,
- *     render: h => h(App)
- *   })
- *   console.log(app)
- * })*/
+import Vue from 'vue'
+import App from '../app.vue'
+ document.addEventListener('DOMContentLoaded', () => {
+  const el = document.body.appendChild(document.createElement('hello'))
+  const app = new Vue({
+    el,
+    render: h => h(App)
+  })
+   console.log(app)
+})
  // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
 // need to always use single file components.
@@ -22,19 +22,22 @@
 // Add <%= javascript_pack_tag 'hello_vue' %> to your layout
 // Then add this markup to your html template:
 //
-
- import Vue from 'vue/dist/vue.esm'
- import App from '../app.vue'
-
- document.addEventListener('DOMContentLoaded', () => {
-   const app = new Vue({
-     el: '#hello',
-     data: {
-       message: "Can you say hello?"
-     },
-     components: { App }
-   })
- })
+// <div id='hello'>
+//   {{message}}
+//   <app></app>
+// </div>
+ // import Vue from 'vue/dist/vue.esm'
+// import App from '../app.vue'
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   const app = new Vue({
+//     el: '#hello',
+//     data: {
+//       message: "Can you say hello?"
+//     },
+//     components: { App }
+//   })
+// })
 //
 //
 //
