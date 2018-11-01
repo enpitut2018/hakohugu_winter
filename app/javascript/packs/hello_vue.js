5c01344a9555617d2957c081624efb271d7a9a7c
kousukeuo <s1611427@s.tsukuba.ac.jp>
2018-11-01 16:22:34 +0900

    herokuデプロイテスト

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,63 +1,45 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
-// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
-// to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
-import Vue from 'vue'
-import App from '../app.vue'
- document.addEventListener('DOMContentLoaded', () => {
+import Vue from 'vue/dist/vue.esm';
+import axios from 'axios';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  //変数argにgetメソッドのパラメータを格納
+  //arg.template_idとしてアクセス。
+  var arg = new Object;
+  var pair=location.search.substring(1).split('&');
+  for(var i=0;pair[i];i++) {
+    var kv = pair[i].split('=');
+    arg[kv[0]]=kv[1];
+  }
+
+
+  //ここからvueインスタンス
   const app = new Vue({
-    el: "#app",
-    render: h => h(App)
-  })
-   console.log(app)
-})
- // The above code uses Vue without the compiler, which means you cannot
-// use Vue to target elements in your existing html templates. You would
-// need to always use single file components.
-// To be able to target elements in your existing html/erb templates,
-// comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-// Then add this markup to your html template:
-//
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
- // import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
-//
-//
-//
-// If the using turbolinks, install 'vue-turbolinks':
-//
-// yarn add 'vue-turbolinks'
-//
-// Then uncomment the code block below:
-//
-// import TurbolinksAdapter from 'vue-turbolinks'
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// Vue.use(TurbolinksAdapter)
-//
-// document.addEventListener('turbolinks:load', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
+    el: '#app',
+    data: {
+      answer: "",
+      note: "",
+      question: "",
+      count: 0
+    },
+
+    created: function () {
+      axios.get(`api/questions/${arg.template_id}`)
+           .then(res => {
+             this.question = res.data;
+           });
+    },
+
+    methods: {
+      addAnswerToNote: function (){
+        this.note += ("Q" + this.question[this.count].qtext);
+        this.note += "\n";
+        this.note += this.answer;
+        this.note += "\n";
+        this.answer = "";
+        this.count += 1;
+      }
+    }
+  });
+});
