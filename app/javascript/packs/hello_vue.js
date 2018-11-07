e551410426a68b7f7e81071511e38045b846043e
kousukeuo <s1611427@s.tsukuba.ac.jp>
2018-11-08 00:08:30 +0900

    webpackの設定を変更.vueのエイリアスをvue/dist/vue.jsに

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -2,5 +2,9 @@ import Vue from 'vue'
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue(App).$mount('#app')
+  new Vue({
+    el: '#app',
+    template: '<App/>',
+    components: { App }
+  })
 })
