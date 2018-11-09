035c0b0b8f62062232f06da7d04e127b7b53c1eb
HinakoIzumi <crossism.libra@gmail.com>
2018-11-10 01:10:40 +0900

    Documentの本文カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_045416) do
+ActiveRecord::Schema.define(version: 2018_11_09_155933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2018_11_07_045416) do
     t.integer "scope"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["template_id"], name: "index_documents_on_template_id"
     t.index ["user_id"], name: "index_documents_on_user_id"
   end
