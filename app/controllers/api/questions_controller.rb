4c0d1e9ac7952d78c818630781b46e1c7cb2adaf
HinakoIzumi <crossism.libra@gmail.com>
2018-11-09 09:51:12 +0900

    データベース改造に伴いqtext取得方法を変更

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,7 +1,7 @@
 class Api::QuestionsController < ApplicationController
    def show
 	  template_id = params[:template_id]
-	  @questions = Template.find(template_id).contains
+	  @questions = Template.find(template_id).questions
      render 'show', formats: 'json', handlers: 'jbuilder'
    end
 end
