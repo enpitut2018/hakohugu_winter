6f4cff3bb1e35f63ea0e491227110a8dd4445579
Ayaka Ebisu <twill.fig1125@gmail.com>
2018-11-20 19:14:42 +0900

    カテゴリのシード値を記入

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 User.create!(name:'User1',email:'user1@gmail.com')
 User.create!(name:'User2',email:'user2@gmail.com')
 Category.create!(name:'研究')
+Category.create!(name:'自己分析')
 
 Template.create!(title:'研究アイデア出しノート',user_id:1,category_id:1,scope:1)
 Template.create!(title:'自己分析',user_id:1,category_id:1,scope:1)
