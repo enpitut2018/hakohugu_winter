129d8e5896454a522cbdd6a3ceaebb32b53de783
Ayaka Ebisu <twill.fig1125@gmail.com>
2018-11-20 13:44:11 +0900

    テンプレートと質問作成フォームのフロント実装

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -3,8 +3,15 @@ class TemplatesController < ApplicationController
   end
 
   def new
+    @template=Template.new
   end
 
   def create
+    @template=Template.new(template_params)
+  end
+
+  private
+  def template_params
+    params.require(:template).permit(:title, questions_attributes: [:qtext, :qdetail, :example, :_destroy] )
   end
 end
