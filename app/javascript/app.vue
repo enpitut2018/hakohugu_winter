4f8e6e825b69f2f345d1a534c7d28d26d76cd549
kousukeuo <s1611427@s.tsukuba.ac.jp>
2018-12-05 13:21:17 +0900

    質問の途中で保存すると保存できないバグの修正

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,63 +1,68 @@
 <template>
-<div id = "app">
-  <div class="form-row mb-md-3">
-    <div class="col">
-      <input type="text" class="form-control" id="Input" :value="title" disabled="disabled"> 
-    </div>
-    <div class="col">
-      <button type="button" class="btn btn-success" @click="saveNote" >ノートを保存</button>
-    </div>
-  </div>
-
-  <ul class="nav nav-tabs">
-    <li class="nav-item">
-      <a href="#tab1" @click="viewChange" class="nav-link active" data-toggle="tab">質問に答える</a>
-    </li>
-    <li class="nav-item">
-      <a href="#tab2" @click="viewChange" class="nav-link" data-toggle="tab">回答の編集</a>
-    </li>
-  </ul>
-
-  <div class="tab-content">
-    <div id="tab1" class="tab-pane active">
-      <div id="conversation">
-        <template v-if="questions" v-for="conversationLog in conversationLogs">
-          <div class="question-balloon">
-            <p>{{conversationLog.question}}</p>
-          </div>
-          <br>
-          <div v-if="conversationLog.answer" class="answer-balloon float-right">
-            <p>{{conversationLog.answer}}</p>
-          </div>
-          <br><br><br>
-        </template>
+  <div id="app">
+    <div class="form-row mb-md-3">
+      <div class="col">
+        <input type="text" class="form-control" id="Input" :value="title" disabled="disabled">
       </div>
-
-      <div id="inputText">
-        <textarea v-model="answer" placeholder="解答を入力" style="width:100%;height:100%;"></textarea>
+      <div class="col">
+        <button type="button" class="btn btn-success" @click="saveNote">ノートを保存</button>
       </div>
+    </div>
+
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a href="#tab1" @click="viewChange" class="nav-link active" data-toggle="tab">質問に答える</a>
+      </li>
+      <li class="nav-item">
+        <a href="#tab2" @click="viewChange" class="nav-link" data-toggle="tab">回答の編集</a>
+      </li>
+    </ul>
+
+    <div class="tab-content">
+      <div id="tab1" class="tab-pane active">
+        <div id="conversation">
+          <template v-if="questions" v-for="conversationLog in conversationLogs">
+            <div class="question-balloon">
+              <p>{{conversationLog.question}}</p>
+            </div>
+            <br>
+            <div v-if="conversationLog.answer" class="answer-balloon float-right">
+              <p>{{conversationLog.answer}}</p>
+            </div>
+            <br>
+            <br>
+            <br>
+          </template>
+        </div>
+
+        <div id="inputText">
+          <textarea v-model="answer" placeholder="解答を入力" style="width:100%;height:100%;"></textarea>
+        </div>
 
         <!-- 回答ボタンの横に次の質問へスキップするボタンを追加 -->
-      <div id="transmission">
-        <div class="transmissionBox">
-          <div class="row">
-            <div id="transmissionMessage" class="col-sm-10 messageBox-col">
-              <button @click="transmissionMessage" type="button" class="btn btn-success btn-block">回答</button>
-            </div>
-            <div id="skipQuestion" class="col-sm-2 skipBox-col">
-              <button @click="skipQuestion" type="button" class="btn btn-primary btn-block">Skip</button>
+        <div id="transmission">
+          <div class="transmissionBox">
+            <div class="row">
+              <div id="transmissionMessage" class="col-sm-10 messageBox-col">
+                <button
+                  @click="transmissionMessage"
+                  type="button"
+                  class="btn btn-success btn-block"
+                >回答</button>
+              </div>
+              <div id="skipQuestion" class="col-sm-2 skipBox-col">
+                <button @click="skipQuestion" type="button" class="btn btn-primary btn-block">Skip</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-    </div>
-
-    <div id="tab2" class="tab-pane">
-      <textarea id="MyID"></textarea>
+      <div id="tab2" class="tab-pane">
+        <textarea id="MyID"></textarea>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 
@@ -192,11 +197,7 @@ export default {
       }
     },
     saveNote: function() {
-      if (this.note_flag == false) {
-        this.viewChange();
-      }
-
-      let document = simplemde.value();
+      let document = this.note;
       axios
         .patch("", {
           content: document
