d13915f2c6153cd0facc4131f0f7f34d95c09f3f
kousukeuo <s1611427@s.tsukuba.ac.jp>
2018-12-10 19:07:36 +0900

    保存のバグの修正

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -44,7 +44,11 @@
           <div class="transmissionBox">
             <div class="row">
               <div id="transmissionMessage" class="col-sm-10 messageBox-col">
-                <button @click="transmissionMessage" type="button" class="btn btn-success btn-block">回答</button>
+                <button
+                  @click="transmissionMessage"
+                  type="button"
+                  class="btn btn-success btn-block"
+                >回答</button>
               </div>
               <div id="skipQuestion" class="col-sm-2 skipBox-col">
                 <button @click="skipQuestion" type="button" class="btn btn-primary btn-block">スキップ</button>
@@ -84,57 +88,111 @@ export default {
       count_e: 0, //例を出す時のカウント変数
       conversationLogs: [],
       tutorials: [
-        '',
-        '',
-        '回答の一覧は回答の編集のタブをクリックすることでいつでも編集できます。',
-        '質問の意味がわからない方は「くわしく」と回答欄に入力してください。私が質問の意図、意味をお答えします。',
-        '他の回答例を参考にしたい、という場合には「例えば」と入力してくだされば、質問に対する回答例を提示させて頂きます。',
-        'どうしても質問に答えられないという方はスキップボタンを押してください。その質問を飛ばして次の質問に行きます。',
-        'これらの機能は「ヘルプ」と回答に入力することでいつでも見ることができます。',
-        '長くなってきましたので、そろそろ始めましょうか。'
+        "",
+        "",
+        "回答の一覧は回答の編集のタブをクリックすることでいつでも編集できます。",
+        "質問の意味がわからない方は「くわしく」と回答欄に入力してください。私が質問の意図、意味をお答えします。",
+        "他の回答例を参考にしたい、という場合には「例えば」と入力してくだされば、質問に対する回答例を提示させて頂きます。",
+        "どうしても質問に答えられないという方はスキップボタンを押してください。その質問を飛ばして次の質問に行きます。",
+        "これらの機能は「ヘルプ」と回答に入力することでいつでも見ることができます。",
+        "長くなってきましたので、そろそろ始めましょうか。"
       ],
-      note_flag: false,
       tutorial_flag: true
     };
   },
 
-  created: function() {
+  mounted: function() {
     let path = location.pathname.split("/");
     let documentNumber = path[2];
-
-    axios.get(`/api/questions/${documentNumber}`).then(res => {
-      this.questions = res.data.questions;
-      this.title = res.data.title;
-      this.note = res.data.content;
-      this.tutorials[0] = 'こんにちは、私は' + res.data.temp_title + 'です。あなたが' + res.data.topic + 'について考えるサポートをさせて頂きます。会話を進めるには回答ボタンを押してください。チュートリアルをスキップしたい方はスキップボタンを押してください。';
-      this.tutorials[1] = 'これから私' + res.data.temp_title + 'が' + res.data.topic + 'について質問していきます。質問と、あなたが入力した回答はノートとして成形されます。質問は全部で' + this.questions.length + '問です。';
-      if (this.note == null) {
-        this.note = "";
-      }
-      this.conversationLogs.push({
-        question: this.tutorials[0]
+    axios
+      .get(`/api/questions/${documentNumber}`)
+      .then(res => {
+        let that = this;
+        return this.storeResponseInVue(res, that);
+      })
+      .then(stored => {
+        let that = this;
+        return this.new_MDE(that);
+      })
+      .then(MEDCreated => {
+        let that = this;
+        simplemde.codemirror.on("change", function() {
+          that.note = simplemde.value();
+        });
       });
-    });
   },
 
   methods: {
+    //mounted のときに呼ばれる関数1
+    storeResponseInVue: function(res, that) {
+      return new Promise(function(resolve) {
+        that.questions = res.data.questions;
+        that.title = res.data.title;
+        that.note = res.data.content;
+        that.tutorials[0] =
+          "こんにちは、私は" +
+          res.data.temp_title +
+          "です。あなたが" +
+          res.data.topic +
+          "について考えるサポートをさせて頂きます。会話を進めるには回答ボタンを押してください。チュートリアルをスキップしたい方はスキップボタンを押してください。";
+        that.tutorials[1] =
+          "これから私" +
+          res.data.temp_title +
+          "が" +
+          res.data.topic +
+          "について質問していきます。質問と、あなたが入力した回答はノートとして成形されます。質問は全部で" +
+          that.questions.length +
+          "問です。";
+        if (that.note == null) {
+          that.note = "";
+        }
+        that.conversationLogs.push({
+          question: that.tutorials[0]
+        });
+        resolve();
+      });
+    },
+    //mounted のときに呼ばれる関数2
+    new_MDE: function(that) {
+      return new Promise(function(resolve) {
+        simplemde = new SimpleMDE({
+          element: document.getElementById("MyID"),
+          initialValue: that.note,
+          forceSync: true, //エディタの入力値をdocument.getElementById("MyID").valueで取得できるようになる
+          autofocus: true //エディタに自動フォーカスする
+        });
+        resolve();
+      });
+    },
     addAnswerToNote: function() {
       this.note +=
         `## Q${this.count + 1}` + this.questions[this.count].qtext + "\n";
       this.note += "\t" + this.answer + "\n";
+      simplemde.value(this.note);
+      setTimeout(function() {
+        simplemde.codemirror.refresh();
+      }, 1);
     },
     transmissionMessage: function() {
       //会話ログに解答を格納
       if (this.questions[this.count]) {
-        this.$set(this.conversationLogs[this.count_t + this.count + this.count_d + this.count_e], "answer", this.answer);
+        this.$set(
+          this.conversationLogs[
+            this.count_t + this.count + this.count_d + this.count_e
+          ],
+          "answer",
+          this.answer
+        );
       }
       //チュートリアル中と本番の質問で場合分け
-      if (this.tutorial_flag && this.count_t<this.tutorials.length){ //チュートリアル中は回答をノートに記録しない
-        this.count_t += 1 ;
-      }else{
+      if (this.tutorial_flag && this.count_t < this.tutorials.length) {
+        //チュートリアル中は回答をノートに記録しない
+        this.count_t += 1;
+      } else {
         this.tutorial_flag = false;
         //特殊回答による分岐
-        if (this.answer == '詳しく' || this.answer == 'くわしく') { //詳しくorくわしくで詳細を表示
+        if (this.answer == "詳しく" || this.answer == "くわしく") {
+          //詳しくorくわしくで詳細を表示
           this.conversationLogs.push({
             question: this.questions[this.count].qdetail
           });
@@ -144,7 +202,8 @@ export default {
             this.scrollToEnd("#conversation");
           });
           return false;
-        }else if (this.answer == '例えば' || this.answer == 'たとえば') { //例えばorたとえばで例を表示
+        } else if (this.answer == "例えば" || this.answer == "たとえば") {
+          //例えばorたとえばで例を表示
           this.conversationLogs.push({
             question: this.questions[this.count].example
           });
@@ -153,8 +212,8 @@ export default {
           this.$nextTick(function() {
             this.scrollToEnd("#conversation");
           });
-　　　　　　return false;
-        }else{
+          return false;
+        } else {
           this.addAnswerToNote();
           this.answer = "";
           //次の質問に進める。
@@ -163,11 +222,14 @@ export default {
       }
       //次の質問を会話ログに格納
       if (this.questions[this.count]) {
-        if ( this.tutorial_flag==false || this.count_t>=this.tutorials.length){
+        if (
+          this.tutorial_flag == false ||
+          this.count_t >= this.tutorials.length
+        ) {
           this.conversationLogs.push({
             question: this.questions[this.count].qtext
           });
-        }else{
+        } else {
           this.conversationLogs.push({
             question: this.tutorials[this.count_t]
           });
@@ -191,10 +253,11 @@ export default {
       this.note += "\t" + "" + "\n";
     },
     skipQuestion: function() {
-      if (this.tutorial_flag) { //チュートリアル中に押されたら、チュートリアルを中止
+      if (this.tutorial_flag) {
+        //チュートリアル中に押されたら、チュートリアルを中止
         this.tutorial_flag = false;
         this.count_t += 1;
-      }else{
+      } else {
         //会話ログに「次の質問」と格納
         if (this.questions[this.count]) {
           this.$set(this.conversationLogs[this.count], "answer", "次の質問は?");
@@ -229,23 +292,9 @@ export default {
       container.scrollTop = container.scrollHeight;
     },
     viewChange: function() {
-      if (this.note_flag == false) {
-        simplemde = new SimpleMDE({
-          element: document.getElementById("MyID"),
-          forceSync: true, //エディタの入力値をdocument.getElementById("MyID").valueで取得できるようになる
-          autofocus: true //エディタに自動フォーカスする
-        });
-        simplemde.value(this.note);
-        setTimeout(function() {
-          simplemde.codemirror.refresh();
-        }, 1);
-        this.note_flag = true;
-      } else {
-        simplemde.value(this.note);
-        setTimeout(function() {
-          simplemde.codemirror.refresh();
-        }, 1);
-      }
+      setTimeout(function() {
+        simplemde.codemirror.refresh();
+      }, 1);
     },
     saveNote: function() {
       let document = this.note;
