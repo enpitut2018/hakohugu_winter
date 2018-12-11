80b3826c8910ce92842e00fa93c1435d4b5789ab
Ayaka Ebisu <twill.fig1125@gmail.com>
2018-12-11 20:31:48 +0900

    ノートの削除

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -24,16 +24,25 @@ class DocumentsController < ApplicationController
 
   def update
     @document = Document.find(params[:id])
-    @document.update_attributes(update_params)    
+    @document.update_attributes(update_params)
+  end
+
+  def destroy
+    if Document.find(params[:id]).destroy
+      redirect_to documents_path
+    else
+      redirect_to documents_path
+    end
+
   end
 
    private
 
     def update_params
-      params.require(:document).permit(:content)                             
+      params.require(:document).permit(:content)
     end
 
     def document_params
-      params.require(:document).permit(:title,:user_id,:template_id)                             
+      params.require(:document).permit(:title,:user_id,:template_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     get '/' , to: 'application#index'
     resources :users, :only => [:new,:create]
     resources :sessions, :only => [:new,:create]
-    resources :documents, :only => [:index,:new,:show,:create,:update]
+    resources :documents, :only => [:index,:new,:show,:create,:update,:destroy]
     resources :templates, :only => [:index,:new,:show,:create,:edit,:update,:destroy]
 
     namespace :api do
