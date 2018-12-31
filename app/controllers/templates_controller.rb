87f86fa9ae7d4d88cd093fdd64cdc1117660a4c7
kousukeuo <s1611427@s.tsukuba.ac.jp>
2019-01-01 01:03:09 +0900

    カテゴリ(キーワード)をテキストにて作成可能になりました

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -2,7 +2,7 @@ class TemplatesController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @templates=Template.paginate(page: params[:page],:per_page => 4).search(params[:search]).where(scope: 1)
+    @templates=Template.paginate(page: params[:page],:per_page => 8).search(params[:search]).where(scope: 1)
     @user = User.find(current_user.id)
     @my_templates_unreleased=@user.templates.where(scope: 0)
     @my_templates_released=@user.templates.where(scope: 1)
@@ -10,6 +10,7 @@ class TemplatesController < ApplicationController
 
   def new
     @template=Template.new
+    @category=Category.new
     @submit='作成'
   end
 
@@ -21,17 +22,25 @@ class TemplatesController < ApplicationController
   end
 
   def create
-    @template=Template.new(template_params)
-    @template.scope = 0
-    if @template.save
-      redirect_to templates_path
+    @category=Category.new(category_params)
+    if @category.save
+      @template=Template.new(template_params)
+      @template.category_id = @category.id
+      @template.scope = 0
+      if @template.save
+        redirect_to templates_path
+      else
+        @category.destroy
+        redirect_to new_template_path, alert: "アシスタントのタイトル、または概要を入力してください。"
+      end
     else
-      render 'new'
+       redirect_to new_template_path, alert: "カテゴリを入力してください。"
     end
   end
 
   def edit
     @template=Template.find(params[:id])
+    @category=Category.find(@template.category.id)
     @submit='更新'
   end
 
@@ -39,10 +48,14 @@ class TemplatesController < ApplicationController
     @template=Template.find(params[:id])
     @category=Category.find(@template.category_id)
     @questions=Question.where(template_id: @template.id)
-    if @template.update_attributes(template_params)
-      render 'show'
+    if @category.update_attributes(category_params)
+      if @template.update_attributes(template_params)
+        render 'show'
+      else
+        redirect_to edit_template_path, alert: "アシスタントのタイトル、概要、またはカテゴリを入力してください。"
+      end
     else
-      render 'edit'
+      redirect_to edit_template_path, alert: "アシスタントのタイトル、概要、またはカテゴリを入力してください。"
     end
   end
 
@@ -68,6 +81,10 @@ class TemplatesController < ApplicationController
 
     def template_params
         params.require(:template).permit(:title,:topic,:category_id,:picture,questions_attributes: [:id, :qtext, :qdetail, :example, :_destroy]).merge(user_id: current_user.id)
+    end
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 
   # ログイン済みユーザーかどうか確認
