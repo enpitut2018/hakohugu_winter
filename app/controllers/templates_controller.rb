1d4dd1eeac6430ea86dcdc38ab55e2bffb211902
kousukeuo <s1611427@s.tsukuba.ac.jp>
2019-01-07 15:17:29 +0900

    タイトルと、カテゴリで検索

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -2,7 +2,7 @@ class TemplatesController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @templates=Template.paginate(page: params[:page],:per_page => 8).search(params[:search]).where(scope: 1)
+    @templates=Template.paginate(page: params[:page],:per_page => 8).search(params[:search],params[:select]).where(scope: 1)
     @user = User.find(current_user.id)
     @my_templates_unreleased=@user.templates.where(scope: 0)
     @my_templates_released=@user.templates.where(scope: 1)
