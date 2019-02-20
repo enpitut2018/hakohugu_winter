ca548b54d13398606bb864fcc53e96fdddd46e0a
wikemasa <wikemasa@gmail.com>
2019-02-21 00:35:14 +0900

    add_help_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include SessionsHelper
     before_action :set_header_links_color
-
+    
+    def help
+    end
+    
     def set_header_links_color
         if controller_name == "documents"
             if ["open","assistant","read"].include?(action_name)
@@ -16,6 +19,10 @@ class ApplicationController < ActionController::Base
             else
                  @header_links_color=['','','','color:#ffed7a;','']
             end
+	elsif controller_name == "application"
+            if action_name == "help"
+                @header_links_color=['','','','','color:#ffed7a;']
+	    end
         else
             @header_links_color=['','','','','']
         end
